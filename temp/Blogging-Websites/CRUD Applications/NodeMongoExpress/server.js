d39d10e874f04683515a8b8d09d3d8b334e4f5dd
Abhishek Gupta <abhig0209@gmail.com>
2024-10-22 22:41:39 +0530

    Blogging Websites added

---
+++ temp/Blogging-Websites/CRUD Applications/NodeMongoExpress/server.js
@@ -0,0 +1,28 @@
+require('./models/db');
+const express = require('express');
+const employeeController = require("./controllers/employeeController");
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+
+var app = express();
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+
+app.set("views", path.join(__dirname, "views"));
+
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'mainLayout',
+    layoutsDir: __dirname + '/views/layouts/'
+}));
+app.set("view engine", "hbs");
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express server started at port:${PORT}`));
+
+app.use('/employee', employeeController);
---
+++ temp/Blogging-Websites/Living A Simple Life Blog/README.md
@@ -0,0 +1,16 @@
+## Totally Responsive Website Made With HTML And CSS
+
+## Theme : Living the social life blog
+
+## Final Product Glimpse
+
+<img src="Images/layout2.png" height="500" width="500">
+<img src="Images/layout1.png" height="500" width="500">
+
+## All Layouts
+
+https://xd.adobe.com/spec/75d448ea-569a-4b7e-721b-9bbd3b2b97b9-03e5/grid
+
+## Reference
+
+https://www.youtube.com/watch?v=srvUrASNj0s
